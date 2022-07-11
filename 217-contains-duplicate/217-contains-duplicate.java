69b36ec5bf7acfcbf4daaf2f45e4bf63ae28d879
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-07-11 18:02:13 +0530

    Time: 33 ms (7.01%), Space: 70 MB (42.14%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -9,11 +9,21 @@ class Solution {
 //             s.add(ele);
 //         }
 //         return false;
-        Arrays.sort(nums);
+       /* Arrays.sort(nums);
         for(int  i = 1; i < nums.length ; i++){
             if(nums[i] == nums[i-1]){
                 return true;
             }
+        }
+        return false;*/
+        
+        int n = nums.length;
+        Map<Integer , Integer> hm = new HashMap<>();
+        for(int i  = 0 ; i < n ; i++){
+            if(hm.containsKey(nums[i])){
+                return true;
+            }
+            hm.put(nums[i] , i);
         }
         return false;
     }
