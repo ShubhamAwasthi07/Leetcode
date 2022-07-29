e1de990480efa4050d0aeb2105953bce2c644aa2
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-07-29 19:16:04 +0530

    Time: 76 ms (28.07%), Space: 149.1 MB (49.83%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,34 +1,23 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public int[] maxSlidingWindow(int[] arr, int k) {
         
-        
-        TreeMap<Integer,Integer> hm = new TreeMap<>();
-        int n = nums.length;
-        int[] ans = new int[n - k + 1];
+         Deque<Integer> dq = new ArrayDeque<>();
+        // ArrayList<Integer> ans = new ArrayList<>();
+        int n = arr.length;
+        int[] ans  = new int[n - k + 1];
         int idx = 0;
         
-        for(int i = 0 ; i < k ; i++){
-            hm.put(nums[i] , hm.getOrDefault(nums[i] , 0) + 1);
-        }
-        ans[idx++] = hm.lastKey(); 
-        
-        int start = 0;
-        int end = k;
-        
-        while(end < n){
-            // 1. Remove start
-            if(hm.get(nums[start]) - 1 == 0)
-                hm.remove(nums[start]);
-            else
-                hm.put(nums[start] , hm.get(nums[start]) - 1);
-            start++;
+        for(int i = 0 ; i < n ; i++){
             
-            //2.  add end
-            hm.put(nums[end] , hm.getOrDefault(nums[end], 0) + 1);
-            end++;
+            while(!dq.isEmpty() && i - dq.peekFirst() >= k){
+                dq.removeFirst();
+            }
             
-            //3 . add max to ans
-            ans[idx++] = hm.lastKey();
+            while(!dq.isEmpty() && arr[i] >= arr[dq.peekLast()])
+                dq.removeLast();
+                
+            dq.offerLast(i);
+            if(i >= k - 1) ans[idx++] = arr[dq.peekFirst()];
         }
         return ans;
     }
