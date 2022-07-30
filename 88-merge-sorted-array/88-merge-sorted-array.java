956202233714ad1c70db6741c67b520eef0a3b3b
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-07-30 22:21:58 +0530

    Time: 1 ms (36.95%), Space: 43.2 MB (15.08%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j = m;
+        for(int i = 0; i < n ; i++){
+            nums1[j] = nums2[i];
+            j++;
+        }
+        Arrays.sort(nums1);
+    }
+}
