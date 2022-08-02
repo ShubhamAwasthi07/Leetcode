c0614605314355762dfb408ba5d3523dd3389bb2
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-08-02 16:07:14 +0530

    Time: 10 ms (84.43%), Space: 48.4 MB (71.16%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,14 +1,15 @@
 class Solution {
     public int firstUniqChar(String s) {
+      char[] hm = new char[26];
         
-        char[] hm = new char[26];
-        
-        for(int i = 0 ; i < s.length(); i++){
+        for(int i = 0 ; i < s.length() ; i++){
             char ch = s.charAt(i);
             hm[ch - 'a']++;
         }
-        for(int i = 0 ; i < s.length() ; i++){
-                if(hm[s.charAt(i) - 'a'] == 1) return i;
+        for(int i = 0; i < s.length() ; i++){
+            if(hm[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
         }
         return -1;
     }
