dc794cf21f7e379d87184dcbddded4261de13bed
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-08-06 06:26:46 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (88.61%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -14,19 +14,15 @@
  * }
  */
 class Solution {
-    
-    void preorder(TreeNode root , List<Integer> ans){
-        if(root == null) return ;
-        
+     void preorder(TreeNode root , List<Integer> ans){
+        if(root == null) return;
         ans.add(root.val);
         preorder(root.left , ans);
         preorder(root.right , ans);
     }
     public List<Integer> preorderTraversal(TreeNode root) {
-        
         List<Integer> ans = new ArrayList<>();
-        preorder(root , ans);
+         preorder(root , ans);
         return ans;
-        
     }
 }
