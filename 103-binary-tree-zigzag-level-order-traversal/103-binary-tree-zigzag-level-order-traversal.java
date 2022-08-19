f672c15ae9370704705e8cc3569aaa49356b32a6
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-08-19 19:33:33 +0530

    Time: 1 ms (96.37%), Space: 42.3 MB (81.53%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -15,38 +15,37 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Stack<TreeNode> curr = new Stack();
-        Stack<TreeNode> next = new Stack();
-        
         List<List<Integer>> ans = new ArrayList<>();
-        if(root == null)
-            return ans;
-        curr.push(root);
-        boolean leftToright = true;
-        while(curr.empty() == false){
-            List<Integer> v = new ArrayList<>();
-            
-            while(curr.empty() == false){
-                TreeNode temp = curr.pop();
-                v.add(temp.val);
-                
-                if(leftToright){
-                    if(temp.left != null) next.push(temp.left);
-                    if(temp.right != null) next.push(temp.right);
-                }else{
-                    if(temp.right != null) next.push(temp.right);
-                    if(temp.left != null) next.push(temp.left);
-                }
-            }
-            
-            Stack<TreeNode> tempStack = curr;
-            curr = next;
-            next = tempStack;
-            
-            leftToright = !leftToright;
-            ans.add(v);
-        }
+        if(root == null) return ans;
         
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        
+        s1.push(root);
+        
+        while(!s1.empty() || !s2.empty()){
+            
+            List<Integer> level = new ArrayList<>();
+            while(!s1.empty()){
+                
+                TreeNode temp = s1.pop();
+                if(temp.left != null) s2.push(temp.left);
+                if(temp.right != null) s2.push(temp.right);
+                
+                level.add(temp.val);
+            }
+            ans.add(level);
+            level = new ArrayList<>();
+             while(!s2.empty()){
+                
+                TreeNode temp = s2.pop();
+                 if(temp.right != null) s1.push(temp.right);
+                 if(temp.left != null) s1.push(temp.left);
+                
+                level.add(temp.val);
+            }
+            if(!level.isEmpty()) ans.add(level);
+        }
         return ans;
     }
 }
