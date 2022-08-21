1119b33e1aa3384eee4a3d8f9021241b3bbb727d
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-08-21 20:46:25 +0530

    Time: 29 ms (75.46%), Space: 59.1 MB (54.00%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -4,39 +4,39 @@ class Solution {
         
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        if(n < 3) return ans;
         int target = 0;
         
-        for(int i = 0 ; i < n; i++){
+        if(n < 3) return ans;
+        for(int i = 0 ; i < n ; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
             
-            int start = i + 1;
-            int end = n -1;
-            
-            int reqTarget = target - nums[i];
+            int start = i+1;
+            int end = n-1;
+            int reTarget = target - nums[i];
             
             while(start < end){
                 int sum = nums[start] + nums[end];
                 
-                if(sum == reqTarget){
-                    ans.add(Arrays.asList(nums[i] ,nums[start], nums[end]));
-                    
+                if(sum == reTarget){
+                    ans.add(Arrays.asList(nums[i] , nums[start] , nums[end]));
                     
                     start++;
                     end--;
-            while(start < end && nums[start] ==  nums[start - 1]){
-                start++;
-            }
-            while(start < end && nums[end] == nums[end + 1]){
-                end--;
-            }
                     
+                    while(start < end && nums[start] == nums[start - 1]){
+                        start++;
+                    }
+                    while(start < end && nums[end] == nums[end + 1]){
+                        end--;
+                    }
                 }
-                else if( sum < reqTarget) start++;
-                else end --;
-            } 
-           
+                else if(sum < reTarget) start++;
+                
+                else end--;
+                
+            }
         }
-        return ans ;
+        return ans;
+        
     }
 }
