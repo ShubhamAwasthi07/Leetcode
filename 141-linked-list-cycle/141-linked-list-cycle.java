daebef7138027d0c029a1599d56f24aa29298436
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-08-22 18:41:01 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (85.61%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,7 +11,6 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null) return false;
         ListNode slow = head;
         ListNode fast = head;
         
@@ -19,9 +18,10 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
             
-            if(slow == fast) return true;
+            if(slow == fast){
+                return true;
+            }
         }
-        
         return false;
     }
 }
