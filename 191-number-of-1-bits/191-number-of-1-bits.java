841ea057234195204aebd37ca0481f97ec9a7075
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-08-30 15:20:38 +0530

    Time: 2 ms (19.29%), Space: 41.6 MB (20.75%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -4,7 +4,9 @@ public class Solution {
         int mask = 1;
         int count = 0;
         for(int i = 0 ; i < 32 ; i++){
-            if((n & mask) != 0) count++;
+            if((n & mask) != 0){
+                count++;
+            }
             n = n >> 1;
         }
         return count;
