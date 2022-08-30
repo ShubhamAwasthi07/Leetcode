24d3ee61ed1226240610963d9b7cf313c4cb5294
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-08-30 16:48:37 +0530

    Time: 5 ms (39.00%), Space: 73.1 MB (11.14%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -4,17 +4,21 @@ class Solution {
         int n = nums.length;
         int i = 0;
         int ans = 0;
-        for(int j = 0; j < n ; j++){
+        
+        for(int j = 0 ; j < n ; j++){
             if(nums[j] == 0){
                 k--;
             }
+            
             if(k >= 0){
                 ans = Math.max(ans , j - i + 1);
+                
             }
             else{
                 while(k < 0){
-                    if(nums[i] == 0)
+                    if(nums[i] == 0){
                         k++;
+                    }
                     i++;
                 }
             }
