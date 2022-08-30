9f72302393940b4bef4616e858cd7665fb516615
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-08-30 18:51:26 +0530

    Time: 1 ms (96.43%), Space: 42.7 MB (63.64%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -17,32 +17,26 @@ class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if(root == null) return ans;
-        
         Stack<TreeNode> s1 = new Stack<>();
         Stack<TreeNode> s2 = new Stack<>();
-        
         s1.push(root);
         
         while(!s1.empty() || !s2.empty()){
-            
             List<Integer> level = new ArrayList<>();
             while(!s1.empty()){
-                
                 TreeNode temp = s1.pop();
+                level.add(temp.val);
+                
                 if(temp.left != null) s2.push(temp.left);
                 if(temp.right != null) s2.push(temp.right);
-                
-                level.add(temp.val);
             }
             ans.add(level);
             level = new ArrayList<>();
-             while(!s2.empty()){
-                
+            while(!s2.empty()){
                 TreeNode temp = s2.pop();
-                 if(temp.right != null) s1.push(temp.right);
-                 if(temp.left != null) s1.push(temp.left);
-                
                 level.add(temp.val);
+                if(temp.right != null) s1.push(temp.right);
+                if(temp.left != null) s1.push(temp.left);
             }
             if(!level.isEmpty()) ans.add(level);
         }
