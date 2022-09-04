9340d14171feda7be1ade3ac0facbde2561bb965
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-04 18:49:41 +0530

    Time: 70 ms (5.13%), Space: 88.2 MB (17.58%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.java
+++ 525-contiguous-array/525-contiguous-array.java
@@ -7,21 +7,21 @@ class Solution {
                 nums[i] = -1;
             }
         }
-        Map<Integer , Integer> hm = new HashMap<>();
+       Map<Integer , Integer> hm = new HashMap<>();
         int sum = 0;
-        int len = 0;
         int k = 0;
+        int len = 0;
         for(int i = 0 ; i < n ; i++){
-            sum += nums[i];
+            sum = sum + nums[i];
             
             if(sum == k)
                 len = i + 1;
-            else if(hm.containsKey(sum - k)){
+            
+            if(hm.containsKey(sum - k)){
                 len = Math.max(len , i - hm.get(sum - k));
             }
-            if(!hm.containsKey(sum)){
+            if(!hm.containsKey(sum))
                 hm.put(sum , i);
-            }
         }
         return len;
     }
