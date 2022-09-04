0afe9af0c35c1527ee15fa804a0acbc3b0d0a402
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-04 17:37:14 +0530

    Time: 45 ms (35.46%), Space: 58.7 MB (69.91%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,37 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
         if(n < 3) return ans;
         int target = 0;
         
         for(int i = 0 ; i < n ; i++){
-            if(i > 0 && nums[i-1] == nums[i]) continue;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             
             int start = i+1;
             int end = n - 1;
-            int retarget = target - nums[i];
-            
+            int reTarget = target - nums[i];
+           
             while(start < end){
-                int sum = nums[start] + nums[end];
+              int sum = nums[start] + nums[end];
                 
-                if(sum == retarget){
+                if(reTarget == sum){
                     ans.add(Arrays.asList(nums[i] , nums[start] , nums[end]));
                     start++;
                     end--;
                     
-                    while(start < end && nums[start] == nums[start - 1]){
+                    while(start < end && nums[start] == nums[start - 1])
                         start++;
-                    }
-                    while(start < end && nums[end] == nums[end + 1]){
+                    while(start < end && nums[end] == nums[end + 1])
                         end--;
-                    }
                 }
-                else if(sum < retarget)
-                    start++;
-                else
-                    end--;
+                else if(sum < reTarget) start++;
+                else end--;
             }
         }
         return ans;
