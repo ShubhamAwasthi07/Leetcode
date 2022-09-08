a2be2505f48c6ed7be5c7e8d1e053c0d48a20ea5
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-08 07:43:00 +0530

    Time: 35 ms (86.08%), Space: 44.1 MB (37.68%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.java
+++ 611-valid-triangle-number/611-valid-triangle-number.java
@@ -1,31 +1,23 @@
 class Solution {
     public int triangleNumber(int[] nums) {
-        int n = nums.length;  // calculating the length of an array
-        int count = 0;     // this variable is used to  store the counts of the valid triangle
-        Arrays.sort(nums);   // sorting the Array
-        for(int k = n-1 ; k > 1 ; k--){
-            int i = 0;    // first pointer
-            int j = k - 1;  // second pointer
+        
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = 0;  
+        for(int i = n - 1 ; i >= 0 ; i--){
             
-            while(i < j){
-                if((nums[i] + nums[j]) > nums[k]){
-                    count = count + j - i;
-                    j--;
+            int start = 0;
+            int end = i - 1;
+            
+            while(start < end){
+                if((nums[start] + nums[end]) > nums[i]){
+                    ans = ans + (end - start);
+                    end--;
                 }
-                else{
-                    i++;
-                }
+                else
+                    start++;
             }
         }
-        return count;
+        return ans;
     }
 }
-
-// 2 2 3 4    nums[i] + nums[j] > nums[k]    count += j - i; 
-// 0 1 2 3
-
-// ^   ^ ^    int i = 0;
-//             int j =  n-2   or j = k - 1 
-//                 int k = n -1
-    
-    
