1d4693a7feba3e9d041a9a5815b686d2343fb3a3
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-08 07:36:50 +0530

    Time: 35 ms (86.08%), Space: 41.5 MB (99.63%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.java
+++ 611-valid-triangle-number/611-valid-triangle-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int triangleNumber(int[] nums) {
-        int n = nums.length;
-        int count = 0;
-        Arrays.sort(nums);
+        int n = nums.length;  // calculating the length of an array
+        int count = 0;     // this variable is used to  store the counts of the valid triangle
+        Arrays.sort(nums);   // sorting the Array
         for(int k = n-1 ; k > 1 ; k--){
-            int i = 0;
-            int j = k - 1;
+            int i = 0;    // first pointer
+            int j = k - 1;  // second pointer
             
             while(i < j){
                 if((nums[i] + nums[j]) > nums[k]){
@@ -20,3 +20,12 @@ class Solution {
         return count;
     }
 }
+
+// 2 2 3 4    nums[i] + nums[j] > nums[k]    count += j - i; 
+// 0 1 2 3
+
+// ^   ^ ^    int i = 0;
+//             int j =  n-2   or j = k - 1 
+//                 int k = n -1
+    
+    
