5b5e256e136f1de8ba8e55fdad93fd8e1e25d640
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-09 13:45:49 +0530

    Time: 0 ms (100.00%), Space: 49.9 MB (28.87%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -1,37 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     
     public int findMin(TreeNode root){
-        
         if(root.left == null) return root.val;
+        
         return findMin(root.left);
     }
-    
     public TreeNode deleteNode(TreeNode root, int val) {
         
         if(root == null) return root;
         
         else if(root.val == val){
-               // Case 1
             
-            if(root.left == null && root.right == null){
+            // case 1
+            if(root.left == null && root.right == null)
                 return null;
+            
+            // case 2
+            if(root.left == null || root.right == null){
+                
+                if(root.left == null)
+                    return root.right;
+                
+                return root.left;
             }
             
-        else if(root.left == null || root.right == null){
-            if(root.left == null){
-                return root.right;
-            }
-            return root.left;
+            // Case 3
+            
+            else{
+                int inorderSucc = findMin(root.right);
+                root.val = inorderSucc;
+                root.right = deleteNode(root.right , inorderSucc);
+            }  
+            
         }
-        else{
-            int inorder_succ = findMin(root.right);
-            root.val = inorder_succ;
-            root.right   = deleteNode(root.right , inorder_succ);
-         }   
-     
-        }
-        else if(val > root.val) root.right = deleteNode(root.right , val);
-        else root.left = deleteNode(root.left , val);
+        else if(root.val > val) root.left = deleteNode(root.left , val);
+        else root.right = deleteNode(root.right , val);
+        
         return root;
     }
 }
