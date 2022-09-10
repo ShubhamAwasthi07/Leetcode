1a7ccec84c3a2f1cc972202d6bc52c5ca6feec9b
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-10 17:09:23 +0530

    Time: 55 ms (29.71%), Space: 51.2 MB (86.77%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,23 +1,24 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        int n = nums.length;
-        Map<Integer , Integer> hm = new HashMap<>();
-        
-        int odd = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] % 2 == 1){
-                odd++;
-            }
-            hm.put(odd , hm.getOrDefault(odd , 0) + 1);
-        }
-        
-        int ans = 0;
-        for(int val : hm.keySet()){
-            if(hm.containsKey(val - k)){
-                ans += hm.get(val) * hm.get(val - k);
-            }
-        }
-        ans += hm.getOrDefault(k , 0);
-        return ans;
+public int numberOfSubarrays(int[] nums, int k) {
+int s=0,e=nums.length;
+
+for(int i=0;i<e;i++){
+    if(nums[i]%2!=0){
+        nums[i]=1;
+    }else{
+        nums[i]=0;
     }
+}
+    Map<Integer,Integer>map=new HashMap<>();
+    map.put(0,1);
+    int ans=0,sum=0;
+    for(int i=0;i<nums.length;i++){
+        sum+=nums[i];
+        if(map.containsKey(sum-k)){
+            ans+=map.get(sum-k);
+        }
+        map.put(sum,map.getOrDefault(sum,0)+1);
+    }
+    return ans;
+}
 }
