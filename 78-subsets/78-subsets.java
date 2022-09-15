bdc9e7c95f4228e0ab7d576e037555d7d4ceb7da
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-15 10:23:20 +0530

    Time: 1 ms (89.58%), Space: 42.5 MB (87.84%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,19 +1,22 @@
 class Solution {
     
-    public void backTrack(List<List<Integer>> ans , List<Integer> temp , int[]nums , int start){
+    public void backTrack(List<List<Integer>> ans , List<Integer> temp , int[] nums , int start){
         
         ans.add(new ArrayList<>(temp));
+        
         for(int i = start ; i < nums.length ; i++){
             temp.add(nums[i]);
             backTrack(ans , temp , nums , i + 1);
-            temp.remove(temp.size() - 1);
+            temp.remove(temp.size()-1);
+            
         }
     }
     public List<List<Integer>> subsets(int[] nums) {
-     
-        List<List<Integer>> ans =  new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        backTrack(ans , temp , nums , 0);
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        backTrack(ans , new ArrayList<>() , nums , 0);
         return ans;
+        
+        
     }
 }
