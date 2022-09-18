4ac788498643c6565fc2a4635deeac91d7409964
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-18 19:24:19 +0530

    Time: 5 ms (69.91%), Space: 45 MB (76.31%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,26 +1,25 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
-        backTrack(candidates , target , 0 , new ArrayList());
-        return ans;
-    }
     
-    public void backTrack(int[] candidates , int target , int rejected , ArrayList<Integer> list){
+    List<List<Integer>> ans = new ArrayList<>();
+    public void backTrack(int[] nums , int target , int rejected , List<Integer> temp){
         
         if(target < 0)
-            return;
+            return ;
         
         if(target == 0){
-            ans.add(new ArrayList(list));
+            ans.add(new ArrayList(temp));
             return ;
         }
-        for(int i = rejected ; i < candidates.length ; i++){
-            list.add(candidates[i]);
-            // check if it is taking to me to a desired solution 
-            backTrack(candidates , target - candidates[i] , i , list);
-            
-            list.remove(list.size() - 1);
+        
+        for(int i = rejected ; i < nums.length ; i++){
+            temp.add(nums[i]);
+            backTrack(nums , target - nums[i] , i , temp);
+            temp.remove(temp.size() - 1);
         }
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        backTrack(candidates, target , 0 , new ArrayList<>());
+        
+        return ans;
     }
 }
