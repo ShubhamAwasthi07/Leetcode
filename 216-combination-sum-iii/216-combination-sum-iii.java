520309f5e158abc5eff5318309b51c0a20675406
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-18 20:01:47 +0530

    Time: 1 ms (87.42%), Space: 41.7 MB (52.45%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -1,25 +1,27 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList();
-    
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        int[] arr = new int[9];
-        for (int i = 1; i <= 9; i++) arr[i - 1] = i;
-        backTrack(arr, n, 0, new ArrayList(), k);
-        return res;
-    }
-    
-    public void backTrack(int[] candidates, int target, int rejected, ArrayList<Integer> list, int k) {
-        if (target < 0) return;
-        if (target == 0 && list.size() == k) {
-            res.add(new ArrayList(list));
+    public void backTrack(List<List<Integer>> ans , List<Integer> temp , int[] nums , int target , int start , int k){
+        
+        if(target < 0)
             return;
+        
+        if(target == 0 && temp.size() == k){
+            ans.add(new ArrayList(temp));
         }
         
-        for (int i = rejected; i < candidates.length; i++) {
-            list.add(candidates[i]);
-            // chek if its taking me to a desired solution
-            backTrack(candidates, target - candidates[i], i + 1, list, k);
-            list.remove(list.size() - 1); // actual backtracking
+        for(int i = start ; i < nums.length ; i++){
+            temp.add(nums[i]);
+            
+            backTrack(ans , temp , nums , target - nums[i] , i + 1 , k);
+            temp.remove(temp.size() - 1);
         }
+    }
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        int[] nums = new int[9];
+        for(int i = 1 ; i <= 9 ; i++){
+            nums[i-1] = i;
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        backTrack(ans , new ArrayList<>() , nums , n , 0 , k);
+        return ans;
     }
 }
