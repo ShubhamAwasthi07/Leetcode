ce183bada9ab237956d111dc71ffd82faf152e5f
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-18 12:26:33 +0530

    Time: 7 ms (13.64%), Space: 44.2 MB (50.53%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,17 +1,19 @@
 class Solution {
     public void backTrack(HashSet<List<Integer>> ans , List<Integer> temp , int[] nums , int start){
-        
         ans.add(new ArrayList(temp));
         Arrays.sort(nums);
         for(int i = start ; i < nums.length ; i++){
             temp.add(nums[i]);
             backTrack(ans , temp , nums , i + 1);
             temp.remove(temp.size() - 1);
+            
         }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
         HashSet<List<Integer>> ans = new HashSet();
         backTrack(ans , new ArrayList<>() , nums , 0);
         return new ArrayList(ans);
+        
     }
 }
