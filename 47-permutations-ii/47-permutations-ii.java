84a017ca138e248fdd7c124a5594f8b5d31eab19
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-18 18:02:43 +0530

    Time: 1228 ms (5.01%), Space: 46.6 MB (85.43%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -4,23 +4,22 @@ class Solution {
         
         if(temp.size() == nums.length && !ans.contains(temp)){
             ans.add(new ArrayList(temp));
-            return;
         }
+        
         for(int i = 0 ; i < nums.length ; i++){
             
             if(used[i]) continue;
-            
             used[i] = true;
             temp.add(nums[i]);
             
             backTrack(ans , temp , nums , used);
             
             used[i] = false;
+            
             temp.remove(temp.size() - 1);
         }
     }
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         
         backTrack(ans , new ArrayList<>() , nums , new boolean[nums.length]);
