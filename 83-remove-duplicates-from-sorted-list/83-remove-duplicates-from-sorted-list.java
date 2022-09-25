8271ae70a4e3f596c88ac5589f88eec8de510b07
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-09-25 21:41:55 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (99.06%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -13,13 +13,15 @@ class Solution {
         
         ListNode temp = head;
         if(head == null) return head;
-       while(temp != null && temp.next != null){
-           if(temp.val == temp.next.val){
-               temp.next = temp.next.next;
-           }else{
-               temp = temp.next;
-           }
-       }
+        
+        while(temp != null && temp.next != null){
+            if(temp.val == temp.next.val){
+                temp.next = temp.next.next;
+            }
+            else{
+                temp = temp.next;
+            }
+        }
         return head;
     }
 }
