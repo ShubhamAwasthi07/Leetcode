051b9260cb105bfce9524b094450279a1f168f30
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-10-03 20:57:10 +0530

    Time: 51 ms (83.77%), Space: 50.1 MB (98.63%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.java
+++ 901-online-stock-span/901-online-stock-span.java
@@ -1,23 +1,26 @@
 class StockSpanner {
 
-    private int curr_idx;
-    private Stack<int[]> st;
-    
+   private int idx;
+   private Stack<int[]> st;
     public StockSpanner() {
-        curr_idx = -1;
+        
+        idx = -1;
         st = new Stack<>();
     }
     
     public int next(int price) {
-        while(!st.empty() && price >= st.peek()[0]){
+       
+        while(!st.isEmpty() && price >= st.peek()[0])
             st.pop();
-        }
-        curr_idx++;
-        int ans;
-        if(st.empty()) ans = curr_idx + 1;
-        else  ans = curr_idx-st.peek()[1];
         
-        st.push(new int[] {price , curr_idx});
+        idx++;
+        int ans;
+        if(st.isEmpty())
+            ans = idx + 1;  
+        else 
+            ans = idx - st.peek()[1];
+        
+        st.push(new int[]{price , idx});
         
         return ans;
     }
