62c6c13bf56a22694d7246287c2e768be45dfe78
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-10-10 06:47:07 +0530

    Time: 9 ms (27.33%), Space: 54 MB (9.97%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -14,23 +14,29 @@
  * }
  */
 class Solution {
-    public boolean twoSum(TreeNode root , Set<Integer> set , int target){
+    
+    public boolean twoSum(TreeNode root , Set<Integer> set , int k){
+        
         if(root == null)
             return false;
-        if(set.contains(target - root.val)){
+        
+        if(set.contains(k - root.val))
             return true;
-        }
+        
         else
             set.add(root.val);
+            
+        boolean left = twoSum(root.left , set , k);
+        boolean right = twoSum(root.right , set , k);
+
+        return (left || right);
         
-        boolean left = twoSum(root.left , set , target);
-        boolean right = twoSum(root.right , set , target);
-        return left || right;
     }
     public boolean findTarget(TreeNode root, int k) {
+        
         Set<Integer> set = new HashSet<>();
-        boolean ans = twoSum(root , set , k);
-        return ans;
+        boolean ans =  twoSum(root , set , k);
+        return ans;  
         
     }
 }
