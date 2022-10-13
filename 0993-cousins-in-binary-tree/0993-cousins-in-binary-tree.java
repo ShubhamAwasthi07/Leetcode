8c9a44199c4fed8572b9d36fdb1997a2f7e2c394
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-10-13 07:57:33 +0530

    Time: 2 ms (21.90%), Space: 41.4 MB (76.51%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -21,36 +21,32 @@ class Solution {
         
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
+        
         while(!q.isEmpty()){
+            
             int size = q.size();
             boolean isXexist = false;
             boolean isYexist = false;
             
             for(int i = 0 ; i < size ; i++){
                 TreeNode curr = q.poll();
-                if(curr.val == x)  isXexist = true;
-                if(curr.val == y)  isYexist = true;
+                
+                if(curr.val == x) isXexist = true;
+                if(curr.val == y) isYexist = true;
                 
                 if(curr.left != null && curr.right != null){
-                    if(curr.left.val == x && curr.right.val == y){
+                    
+                    if(curr.left.val == x && curr.right.val == y)
                         return false;
-                    }
                     if(curr.left.val == y && curr.right.val == x)
                         return false;
                 }
                 
-                if(curr.left != null){
-                    q.offer(curr.left);
-                }
-                if(curr.right != null){
-                    q.offer(curr.right);
-                }
+                if(curr.left != null) q.offer(curr.left);
+                if(curr.right != null) q.offer(curr.right);
             }
-            if(isXexist && isYexist)
-               return true;
+            if(isXexist && isYexist) return true;
         }
-        
-            return false;
-        
+        return false;
     }
 }
