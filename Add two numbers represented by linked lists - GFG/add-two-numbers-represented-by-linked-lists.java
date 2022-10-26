ae5789108590f9f5629d8ea87df02dd2de63f1db
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-10-26 12:38:04 +0530

    Added solution - LeetHub

--- Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.java
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.java
@@ -79,7 +79,21 @@ class Node {
 
 class Solution{
     //Function to add two numbers represented by linked list.
-    
+    static Node reverse(Node head){
+        if(head == null)
+            return head;
+        Node curr = head;
+        Node next = null;
+        Node prev = null;
+        
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
     static Node add(Node l1 , Node l2 , int carry){
         
         if(l1 == null && l2 == null){
@@ -90,8 +104,16 @@ class Solution{
                 return new Node(carry);
             }
         }
+        if(l2 == null){
+            if(carry == 0){
+                return l1;
+            }
+            else{
+                Node temp = new Node(carry);
+                return add(l1 , temp , 0);
+            }
+        }
         if(l1 == null){
-            
             if(carry == 0){
                 return l2;
             }
@@ -100,43 +122,22 @@ class Solution{
                 return add(temp , l2 , 0);
             }
         }
-        if(l2 == null){
-            if(carry == 0)
-                return l1;
-            else{
-                Node temp = new Node(carry);
-                return add(l1 , temp , 0);
-            }
-        }
-        
         int sum = l1.data + l2.data + carry;
         int val = sum % 10;
         Node temp = new Node(val);
-        int carryNew = sum / 10;
-        temp.next = add(l1.next , l2.next , carryNew);
+        int NewCarry = sum / 10;
+        temp.next = add(l1.next , l2.next , NewCarry);
         
         return temp;
-    }
-    static Node reverse(Node head){
-        Node curr = head;
-        Node prev = null;
-        Node next = null;
-        
-        while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
     }
     static Node addTwoLists(Node first, Node second){
         // code here
         // return head of sum list
-        Node list1 = reverse(first);
-        Node list2 = reverse(second);
-        Node ans = add(list1 , list2 , 0);
-        Node res = reverse(ans);
-        return res;
+        Node head1 = reverse(first);
+        Node head2 = reverse(second);
+        Node ans = add(head1 , head2 , 0);
+        return reverse(ans);
+        
+       
     }
 }
