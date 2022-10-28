0646951296b2978d83b8a127d00e5c4c296f419f
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-10-28 19:57:37 +0530

    Time: 7 ms (96.04%), Space: 45.4 MB (93.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
+        Map<String , List<String>> hm = new HashMap<>();
         int n = strs.length;
-        Map<String , ArrayList<String>> hm = new HashMap<>();
         
         for(int i = 0 ; i < n ; i++){
+            
             char[] ch = strs[i].toCharArray();
             Arrays.sort(ch);
-            
             String sortedString = new String(ch);
             
             if(!hm.containsKey(sortedString)){
-                hm.put(sortedString , new ArrayList<String>());
+                hm.put(sortedString , new ArrayList<String>()); 
             }
+            hm.get(sortedString).add(strs[i]);
             
-            hm.get(sortedString).add(strs[i]); 
         }
         return new ArrayList<>(hm.values());
     }
