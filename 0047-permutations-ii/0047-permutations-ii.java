bb20852bb313395042641b6991356bb6c90d14b1
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-10-31 08:09:06 +0530

    Time: 957 ms (6.06%), Space: 47.8 MB (65.15%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void backTrack(List<List<Integer>> ans , List<Integer> temp , int[] nums , boolean[] used){
+        
+        if(temp.size() == nums.length && !ans.contains(temp))
+            ans.add(new ArrayList<>(temp));
+            
+        for(int i = 0 ; i < nums.length ; i++){
+            if(used[i])
+                continue;
+            used[i] = true;
+            temp.add(nums[i]);
+            backTrack(ans , temp , nums , used);
+            used[i] = false;
+            temp.remove(temp.size() - 1);
+            
+        }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        backTrack(ans , new ArrayList<>() , nums , new boolean[nums.length]);
+        return ans;
+    }
+}
