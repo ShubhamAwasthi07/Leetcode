eb69b92aa7ef138e03a2b1b8c80352c1a1196601
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-11-23 20:21:00 +0530

    Time: 34 ms (20.93%), Space: 43.3 MB (47.48%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+class Solution {
+    public String addStrings(String num1, String num2) {
+        
+        BigInteger b1=new BigInteger(num1);
+        BigInteger b2=new BigInteger(num2);
+
+        return String.valueOf(b1.add(b2));
+    }
+}
