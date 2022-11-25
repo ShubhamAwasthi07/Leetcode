0becadf9691ec58325ea19414f2113be7ea4e798
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-11-25 18:57:57 +0530

    Time: 33 ms (18.73%), Space: 56 MB (49.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -7,12 +7,12 @@ class Solution {
         for(int i = 0 ; i < nums.length ; i++){
             hm.put(nums[i] , hm.getOrDefault(nums[i] , 0) + 1);
         }
-        int max = 0;
+        // int max = 0;
         for(int val : hm.keySet()){
             if(hm.get(val) > Math.floor(n/2)){
-                max = Math.max(max , val);
+                return val;
             }
         }
-        return max;
+        return -1;
     }
 }
