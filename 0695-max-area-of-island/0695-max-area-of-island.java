4be213017b851da10c0a62332327e10c66155a35
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-12-04 12:25:48 +0530

    Time: 3 ms (88.28%), Space: 47.1 MB (61.67%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -5,8 +5,8 @@ class Solution {
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0){
             return 0;
         }
-            if(grid[i][j] == 0)
-                return 0;
+            // if(grid[i][j] == 0)
+            //     return 0;
         grid[i][j] = 0;
         
         return 1 + dfs(grid , i - 1 , j) + dfs(grid , i + 1 , j) + dfs(grid , i , j - 1) + dfs(grid , i , j + 1);
