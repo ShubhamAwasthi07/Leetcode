0d591fe7c8882d21bf6395b25ecf36afd28e09ea
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2023-02-19 19:22:32 +0530

    Added solution - LeetHub

---
+++ Kth largest element in a stream - GFG/kth-largest-element-in-a-stream.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int k = Integer.parseInt(S[0]);
+            int n = Integer.parseInt(S[1]);
+            
+            String S1[] = read.readLine().split(" ");
+            
+            int[] arr = new int[n];
+            
+            for(int i=0; i<n; i++)
+                arr[i] = Integer.parseInt(S1[i]);
+
+            Solution ob = new Solution();
+            int[] ptr = ob.kthLargest(k,arr,n);
+            for(int i=0; i<n; i++)
+                System.out.print(ptr[i] + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static int[] kthLargest(int k, int[] arr, int n) {
+        // code here
+        
+        int[] ans = new int[n];
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+            return a-b;
+        });
+        
+        for(int i = 0 ; i < n ; i++){
+            
+            if(pq.size() < k)
+                pq.add(arr[i]);
+            
+            else{
+                if(arr[i] > pq.peek()){
+                    pq.remove();
+                    pq.add(arr[i]);
+                }
+            }
+            if(pq.size() < k)
+                ans[i] = -1;
+            else{
+                ans[i] = pq.peek();
+            }
+        }
+        return ans;
+    }
+};
